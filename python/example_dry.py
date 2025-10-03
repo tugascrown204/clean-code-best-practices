41b910952697dff9afebb08a1f174388f0171134
tugascrown204 <gajdertevet@hotmail.com>
2025-10-03 19:51:24 +0330

    feat: initial commit for python/example_dry.py

---
+++ python/example_dry.py
@@ -0,0 +1,7 @@
+def send_email(to, subject, body):
+    # Code to send email
+    pass
+
+# Using the send_email function in multiple places
+send_email('user@example.com', 'Subject 1', 'Body 1')
+send_email('user2@example.com', 'Subject 2', 'Body 2')
