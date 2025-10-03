2d840a09134f2078aef20ea1f7f1e9fb41763bf7
tugascrown204 <gajdertevet@hotmail.com>
2025-10-03 19:52:00 +0330

    feat: initial commit for go/example_dry.go

---
+++ go/example_dry.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func sendEmail(to, subject, body string) {
+    // Code to send email
+}
+
+func main() {
+    sendEmail("user@example.com", "Subject 1", "Body 1")
+    sendEmail("user2@example.com", "Subject 2", "Body 2")
+}
