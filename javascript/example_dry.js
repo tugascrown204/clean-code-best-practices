3cb2177b290bd9b4fa799c2b67c2024473cd2409
tugascrown204 <gajdertevet@hotmail.com>
2025-10-03 19:51:33 +0330

    feat: initial commit for javascript/example_dry.js

---
+++ javascript/example_dry.js
@@ -0,0 +1,7 @@
+function sendEmail(to, subject, body) {
+    // Code to send email
+}
+
+// Using the sendEmail function in multiple places
+sendEmail('user@example.com', 'Subject 1', 'Body 1');
+sendEmail('user2@example.com', 'Subject 2', 'Body 2');
